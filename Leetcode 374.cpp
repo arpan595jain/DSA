e73518b2dae5cbb24a7f48be7d634d5d5459472f
Arpan Jain <arpanjain595@gmail.com>
2024-08-01 10:51:44 +0530

    Create Leetcode 374.cpp

---
+++ Leetcode 374.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+       int start = 1;
+            int end = n;
+            while(start <= end){
+                int mid = start + (end-start) / 2;
+            
+                if(guess(mid) == -1){
+                    end = mid;
+                }
+                else if(guess(mid) == 1){
+                    start = mid+1;
+                }
+                else{
+                    return mid;
+                }
+        }
+        return 0;  
+    }
+};
